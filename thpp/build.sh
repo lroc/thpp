0efb26d6648f6001f306d9c999a3654deb111ff8
lroc <529808348@qq.com>
2016-05-17 23:39:53 +0800

    Update build.sh

--- thpp/build.sh
+++ thpp/build.sh
@@ -16,7 +16,7 @@ if [[ ! -r ./Tensor.h ]]; then
 fi
 
 rm -rf gtest-1.7.0 gtest-1.7.0.zip
-curl -JLO https://googletest.googlecode.com/files/gtest-1.7.0.zip
+curl -JLO https://github.com/lroc/thpp/raw/master/gtest-1.7.0.zip
 if [[ $(sha1sum -b gtest-1.7.0.zip | cut -d' ' -f1) != \
       'f85f6d2481e2c6c4a18539e391aa4ea8ab0394af' ]]; then
   echo "Invalid gtest-1.7.0.zip file" >&2
